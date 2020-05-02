e43bae298bbbc6fcb49d9e5fb85e14de9c2f5b0d
Isaac Batista <batistaferreira2016@gmail.com>
2020-05-02 15:08:33 -0400

    Create project file

---
+++ Literaturese_Isaac/app/src/main/java/com/example/literature_se_isaac/listagem.java
@@ -0,0 +1,14 @@
+package com.example.literature_se_isaac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listagem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem);
+    }
+}
